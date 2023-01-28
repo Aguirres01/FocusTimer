4ba15e55899914dcf43fccd2a78905bd4a76bd23
Marlon Aguirre <andrmarlon@gmail.com>
2023-01-28 16:27:09 -0300

    buttons

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 //DOM => Document Object Model = Event Driven
 
-let play = document.querySelector('.play') //pesquisa pelo seletor
-let pause = document.querySelector('.pause')
+//refatorar = código mais entendível
 
+const buttonPlay = document.querySelector('.play') //pesquisa pelo seletor
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
 //Event-driven
 //callback
 
-play.addEventListener('click', function (){
-    play.classList.add('hide') //adicionou hide no play
-    pause.classList.remove('hide') //removey hide do pause
+buttonPlay.addEventListener('click', function (){
+    buttonPlay.classList.add('hide') //adicionou hide no play
+    buttonPause.classList.remove('hide') //removey hide do pause
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+
 })
-pause.addEventListener('click',function(){
-    pause.classList.add('hide')
-    play.classList.remove('hide')
+buttonPause.addEventListener('click',function(){
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
 })
 
 
-// Programação imperativa
+buttonStop.addEventListener('click', function (){
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function(){
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function(){
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
