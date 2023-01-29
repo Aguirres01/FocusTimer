3f07453040c6253c585c31b4a4750861d0e5adca
Marlon Aguirre <andrmarlon@gmail.com>
2023-01-29 09:52:19 -0300

    adicionando countdown

--- script.js
+++ script.js
@@ -2,22 +2,44 @@
 
 //refatorar = código mais entendível
 
+let minutes //criei variavel sem valor
 const buttonPlay = document.querySelector('.play') //pesquisa pelo seletor
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
-//Event-driven
-//callback
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+function countdown (){
+    setTimeout(function(){
+        let seconds = Number(secondsDisplay.textContent)
+
+        if (seconds <= 0) {
+            seconds = 60
+        }
+
+        secondsDisplay.textContent = seconds -1
+
+        countdown()
+    }, 1000)
+}
+
+
+
 
 buttonPlay.addEventListener('click', function (){
     buttonPlay.classList.add('hide') //adicionou hide no play
     buttonPause.classList.remove('hide') //removey hide do pause
     buttonSet.classList.add('hide')
     buttonStop.classList.remove('hide')
-
+   
+    countdown()
+   
+   
 })
+
 buttonPause.addEventListener('click',function(){
     buttonPause.classList.add('hide')
     buttonPlay.classList.remove('hide')
@@ -39,4 +61,9 @@ buttonSoundOff.addEventListener('click', function(){
 buttonSoundOn.addEventListener('click', function(){
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function(){
+    minutes = prompt('Quantos minutos?')
+    minutesDisplay.textContent = minutes
 })
